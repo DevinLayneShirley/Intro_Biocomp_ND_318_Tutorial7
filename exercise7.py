f2df0e4a9ec9e5d1ad829d43d8e346e2235e15a5
Devin Layne Shirley <devin.layne.shirley@gmail.com>
2017-10-10 21:01:36 -0400

    fixed some graphs, three out of five work now

--- exercise7.py
+++ exercise7.py
@@ -3,7 +3,9 @@
 import pandas 
 import numpy
 from plotnine import *
+
 #1 create two plots that summarize info from fasta file 
+
 # open fasta file
 fasta=open("Lecture11.fasta","r")
 #create lists for storing information about sequences
@@ -41,21 +43,26 @@ seqinfo = pandas.DataFrame(list(zip(sequenceID,sequenceLength,percentGC,meltingT
 fasta.close()
 #check out summary table
 seqinfo.head()
+
 #FIRST GRAPH: HISTOGRAM OF SEQUENCE LENGTHS
-seqlen=ggplot(seqinfo,aes(x="sequenceLength"))
-seqlen+geom_histogram()+theme_classic #UNSUPPORTED OPERAND TYPE?
+seqlen=ggplot(seqinfo,aes('sequenceLength'))
+seqlen+geom_histogram(binwidth=10,fill='cadetblue',color='black')+theme_classic()+ggtitle("Sequence Lengths")
+#https://i.stack.imgur.com/fMx2j.png gives a bunch of colors in python,
+#I chose to make the bars cadetblue with black outlines
+
 #SECOND GRAPH: HISTOGRAM OF GC CONTENT 
-gccont=ggplot(seqinfo,aes(x="percentGC"))
-gccont+geom_histogram()+theme_classic #UNSUPPORTED OPERAND TYPE?
+gccont=ggplot(seqinfo,aes('percentGC'))
+gccont+geom_histogram(binwidth=1,fill='mediumorchid',color='black')+theme_classic()+ggtitle("GC Content of the Sequences")
+
 #2 Create graph relating two variables
 #add data
-geyser=pandas.read_csv("old_faithful_erruptions.csv",header=0)
+geyser=pandas.read_csv("old_faithful_erruptions.txt",sep="\t",header=0)
 #check to make sure data loaded properly 
 geyser.head()
 #create graph with treadline
-ggraph=ggplot(geyser,aes("eruptions","waiting"))
-ggraph+geom_point(x="eruptions",y="waiting") #DOESN'T LIKE THE FLOAT
-ggraph+xlab("Eruption Duration (min)")+ylab("Time Waited (min)")+stat_smooth(method="lm")
+ggraph=ggplot(geyser,aes('eruptions','waiting'))
+ggraph+geom_point(color='firebrick')+xlab("Eruption Duration (min)")+ylab("Time Waited (min)")+stat_smooth(method="lm")#stat_smooth adds the trend line
+
 #3 two figures that summarize data in data.txt
 #load data
 data=pandas.read_csv("data.txt",header=0)
